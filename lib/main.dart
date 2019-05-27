ed90a02ce268251f814aeda459829df03e73b1d5
Antonio <recuperoantonio94@gmail.com>
2019-05-27 18:48:34 +0200

    Validation implemented (non empty values)

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,8 @@ class _SIFormState extends State<SIForm> {
   final _minimumPadding = 5.0;
 
   //Variabili per la logica
-  var _currentItemSelected = ''; //deve essere uguale ad uno dei tipi del menù a tendina
+  var _currentItemSelected =
+      ''; //deve essere uguale ad uno dei tipi del menù a tendina
 
   TextEditingController principalController = TextEditingController();
   TextEditingController roiController = TextEditingController();
@@ -32,85 +33,119 @@ class _SIFormState extends State<SIForm> {
 
   var displayResult = '';
 
+  //Variabili per la validazione del Form
+  var _formKey =
+      GlobalKey<FormState>(); // -> agisce come chiave per il nostro form
+
   @override
-  void initState(){ //funzione predefinita da usare per l'inizializzazione dello stato di un widget -> chiamata quando il widget viene creato
+  void initState() {
+    //funzione predefinita da usare per l'inizializzazione dello stato di un widget -> chiamata quando il widget viene creato
     super.initState();
     _currentItemSelected = _currencies[0];
   }
 
-
-
   @override
   Widget build(BuildContext context) {
-    TextStyle textStyle = Theme
-        .of(context)
-        .textTheme
-        .title;
+    TextStyle textStyle = Theme.of(context).textTheme.title;
 
     return Scaffold(
       //   resizeToAvoidBottomPadding: false, //per evitare overflow dello schermo nella parte inferiore
       appBar: AppBar(
         title: Text('Simple Interest Calculator'),
       ),
-      body: Container(
-        margin: EdgeInsets.all(_minimumPadding * 2),
-        //child: Column(
-        child: ListView(
-          //se si restringe lo schermo (ad es. tastiera o schermo troppo piccolo) mi permette di scorrere la facciata dell'app
-          children: <Widget>[
-            getImageAsset(),
-            Padding(
-                padding: EdgeInsets.only(
-                    top: _minimumPadding, bottom: _minimumPadding),
-                child: TextField(
-                  keyboardType: TextInputType.number,
-                  style: textStyle,
-                  controller: principalController, //collego il campo di input alla variabile controller per farne I/O
-                  decoration: InputDecoration(
-                      labelText: 'Principal',
-                      hintText: 'Enter Principal e.g. 12000',
-                      labelStyle: textStyle,
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20.0))),
-                )),
-            Padding(
-              padding: EdgeInsets.only(
-                  top: _minimumPadding, bottom: _minimumPadding),
-              child: TextField(
-                keyboardType: TextInputType.number,
-                style: textStyle,
-                controller: roiController,
-                decoration: InputDecoration(
-                    labelText: 'Rate of Interest',
-                    hintText: 'In percent',
-                    labelStyle: textStyle,
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20.0))),
-              ),
-            ),
-            Padding(
-                padding: EdgeInsets.only(
-                    top: _minimumPadding, bottom: _minimumPadding),
-                child: Row(
-                  children: <Widget>[
-                    Expanded(
-                      //Expanded utilizzato perchè altrimenti non ci sta tutto nello schermo e l'app crasha
-                        child: TextField(
+      body: Form(
+//        margin: EdgeInsets.all(_minimumPadding * 2), ---> non contenuto in Form -> prima c'era un container
+          //child: Column(
+          key: _formKey,
+          child: Padding(
+            padding: EdgeInsets.all(_minimumPadding * 2),
+            child: ListView(
+              //--->    se si restringe lo schermo (ad es. tastiera o schermo troppo piccolo) mi permette di scorrere la facciata dell'app
+              children: <Widget>[
+                getImageAsset(),
+                Padding(
+                    padding: EdgeInsets.only(
+                        top: _minimumPadding, bottom: _minimumPadding),
+                    //child: TextField(
+                    child: TextFormField(
+                      //Per la validazione dell'input
+                      keyboardType: TextInputType.number,
+                      style: textStyle,
+                      controller: principalController,
+                      validator: (String value) {
+                        //codice per la validazione dell'input
+                        if (value.isEmpty) {
+                          return 'Please enter principal amount';
+                        }
+                      },
+                      //collego il campo di input alla variabile controller per farne I/O
+                      decoration: InputDecoration(
+                          labelText: 'Principal',
+                          hintText: 'Enter Principal e.g. 12000',
+                          labelStyle: textStyle,
+                          errorStyle: TextStyle(
+                            color: Colors.yellowAccent,
+                            fontSize: 15.0
+                          ),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20.0))),
+                    )),
+                Padding(
+                  padding: EdgeInsets.only(
+                      top: _minimumPadding, bottom: _minimumPadding),
+                  child: TextFormField(
+                    keyboardType: TextInputType.number,
+                    style: textStyle,
+                    controller: roiController,
+                    validator:  (String value) {
+                      //codice per la validazione dell'input
+                      if (value.isEmpty) {
+                        return 'Please enter rate of interest';
+                      }
+                    },
+                    decoration: InputDecoration(
+                        labelText: 'Rate of Interest',
+                        hintText: 'In percent',
+                        labelStyle: textStyle,
+                        errorStyle: TextStyle(
+                            color: Colors.yellowAccent,
+                            fontSize: 15.0
+                        ),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20.0))),
+                  ),
+                ),
+                Padding(
+                    padding: EdgeInsets.only(
+                        top: _minimumPadding, bottom: _minimumPadding),
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(
+                            //Expanded utilizzato perchè altrimenti non ci sta tutto nello schermo e l'app crasha
+                            child: TextFormField(
                           keyboardType: TextInputType.number,
                           style: textStyle,
                           controller: termController,
+                          validator: (String value){
+                            if(value.isEmpty){
+                              return 'Please enter the time';
+                            }
+                          },
                           decoration: InputDecoration(
                               labelText: 'Term',
                               hintText: 'Time in years',
                               labelStyle: textStyle,
+                              errorStyle: TextStyle(
+                                color: Colors.yellowAccent
+                              ),
                               border: OutlineInputBorder(
                                   borderRadius: BorderRadius.circular(20.0))),
                         )),
-                    Container(
-                      width: _minimumPadding * 5,
-                    ),
-                    Expanded(
-                        child: DropdownButton<String>(
+                        Container(
+                          width: _minimumPadding * 5,
+                        ),
+                        Expanded(
+                            child: DropdownButton<String>(
                           items: _currencies.map((String value) {
                             return DropdownMenuItem<String>(
                               value: value,
@@ -123,48 +158,57 @@ class _SIFormState extends State<SIForm> {
                             _onDropDownItemSelected(newValueSelected);
                           },
                         ))
-                  ],
-                )),
-            Padding(
-                padding: EdgeInsets.only(
-                    bottom: _minimumPadding, top: _minimumPadding),
-                child: Row(
-                  children: <Widget>[
-                    Expanded(
-                        child: RaisedButton(
-                          child: Text('Calculate', textScaleFactor: 1.5,),
+                      ],
+                    )),
+                Padding(
+                    padding: EdgeInsets.only(
+                        bottom: _minimumPadding, top: _minimumPadding),
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(
+                            child: RaisedButton(
+                          child: Text(
+                            'Calculate',
+                            textScaleFactor: 1.5,
+                          ),
                           color: Theme.of(context).accentColor,
                           textColor: Theme.of(context).primaryColorDark,
                           onPressed: () {
-                            //eseguo la logica per effettuare il calcolo alla pressione del tasto
-                            setState(() {
-                             this.displayResult =  _calculateTotalReturns();
-                            });
+                            if (_formKey.currentState.validate()) {
+                              //per collegarmi al form (chiave unica per questo form) -> se i campi sono validati (vedi campi) -> esegue codice
+                              //eseguo la logica per effettuare il calcolo alla pressione del tasto
+                              setState(() {
+                                this.displayResult = _calculateTotalReturns();
+                              });
+                            }
                           },
                         )),
-                    Expanded(
-                        child: RaisedButton(
+                        Expanded(
+                            child: RaisedButton(
                           color: Theme.of(context).primaryColorDark,
                           textColor: Theme.of(context).primaryColorLight,
-                          child: Text('Reset', textScaleFactor: 1.5,),
+                          child: Text(
+                            'Reset',
+                            textScaleFactor: 1.5,
+                          ),
                           onPressed: () {
                             setState(() {
                               _reset();
                             });
                           },
                         ))
-                  ],
-                )),
-            Padding(
-              padding: EdgeInsets.all(_minimumPadding * 2),
-              child: Text(
-                displayResult,
-                style: textStyle,
-              ),
-            )
-          ],
-        ),
-      ),
+                      ],
+                    )),
+                Padding(
+                  padding: EdgeInsets.all(_minimumPadding * 2),
+                  child: Text(
+                    displayResult,
+                    style: textStyle,
+                  ),
+                )
+              ],
+            ),
+          )),
     );
   }
 
@@ -182,23 +226,27 @@ class _SIFormState extends State<SIForm> {
     );
   }
 
-  void _onDropDownItemSelected(String newValueSelected) { //setta il valore scelto nel men a tendina
+  void _onDropDownItemSelected(String newValueSelected) {
+    //setta il valore scelto nel men a tendina
     setState(() {
       this._currentItemSelected = newValueSelected;
     });
   }
 
   String _calculateTotalReturns() {
-    double principal = double.parse(principalController.text); //leggo il testo convertendolo in double
-    double roi = double.parse(roiController.text); //leggo il testo convertendolo in double
-    double term = double.parse(termController.text); //leggo il testo convertendolo in double
+    double principal = double.parse(
+        principalController.text); //leggo il testo convertendolo in double
+    double roi = double.parse(
+        roiController.text); //leggo il testo convertendolo in double
+    double term = double.parse(
+        termController.text); //leggo il testo convertendolo in double
 
-    double totalAmountPayable = principal + (principal * roi * term) /100;
+    double totalAmountPayable = principal + (principal * roi * term) / 100;
 
-    String result = 'After $term years, your investment will be worth $totalAmountPayable $_currentItemSelected';
+    String result =
+        'After $term years, your investment will be worth $totalAmountPayable $_currentItemSelected';
 
     return result;
-
   }
 
   void _reset() {
