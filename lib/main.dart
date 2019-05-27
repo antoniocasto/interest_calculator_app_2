95da3b93b69d766d7547e443cf2a4be208b0158f
Antonio <recuperoantonio94@gmail.com>
2019-05-27 17:23:31 +0200

    some updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,130 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Simple Interest Calculator App',
+    home: SIForm(),
+  ));
+}
+
+class SIForm extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _SIFormState();
+  }
+}
+
+class _SIFormState extends State<SIForm> {
+  var _currencies = ['Dollari', 'Euro', 'Pounds'];
+  final _minimumPadding = 5.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+   //   resizeToAvoidBottomPadding: false, //per evitare overflow dello schermo nella parte inferiore
+      appBar: AppBar(
+        title: Text('Simple Interest Calculator'),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(_minimumPadding * 2),
+        //child: Column(
+          child: ListView( //se si restringe lo schermo (ad es. tastiera o schermo troppo piccolo) mi permette di scorrere la facciata dell'app
+          children: <Widget>[
+            getImageAsset(),
+            Padding(
+                padding: EdgeInsets.only(
+                    top: _minimumPadding, bottom: _minimumPadding),
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      labelText: 'Principal',
+                      hintText: 'Enter Principal e.g. 12000',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20.0))),
+                )),
+            Padding(
+              padding: EdgeInsets.only(
+                  top: _minimumPadding, bottom: _minimumPadding),
+              child: TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: 'Rate of Interest',
+                    hintText: 'In percent',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0))),
+              ),
+            ),
+            Padding(
+                padding: EdgeInsets.only(
+                    top: _minimumPadding, bottom: _minimumPadding),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                        //Expanded utilizzato perchè altrimenti non ci sta tutto nello schermo e l'app crasha
+                        child: TextField(
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                          labelText: 'Term',
+                          hintText: 'Time in years',
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20.0))),
+                    )),
+                    Container(
+                      width: _minimumPadding * 5,
+                    ),
+                    Expanded(
+                        child: DropdownButton<String>(
+                      items: _currencies.map((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                      value: 'Euro',
+                      onChanged: (String newValueSelected) {
+                        //eseguo codice alla selezione
+                      },
+                    ))
+                  ],
+                )),
+            Padding(
+                padding: EdgeInsets.only(bottom: _minimumPadding, top: _minimumPadding),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                        child: RaisedButton(
+                      child: Text('Calculate'),
+                      onPressed: () {},
+                    )),
+                    Expanded(
+                        child: RaisedButton(
+                      child: Text('Reset'),
+                      onPressed: () {},
+                    ))
+                  ],
+                )),
+            Padding(
+                padding: EdgeInsets.all(_minimumPadding*2),
+                child: Text('Todo Text'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget getImageAsset() {
+    AssetImage assetImage = AssetImage('images/interest.png');
+    Image image = Image(
+      image: assetImage,
+      width: 125.0,
+      height: 125.0,
+    );
+
+    return Container(
+      child: image,
+      margin: EdgeInsets.all(_minimumPadding * 10.0),
+    );
+  }
+}
